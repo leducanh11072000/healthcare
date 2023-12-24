4b896d202bae606882f8f34684a9ac8c4eac3941
DucAnhWorkPlace <leducanh1107.work@gmail.com>
2023-12-24 12:31:10 +0700

    23/12

--- HealthCareApplication/src/main/java/com/example/healthcareapplication/Utils/DataUtils.java
+++ HealthCareApplication/src/main/java/com/example/healthcareapplication/Utils/DataUtils.java
@@ -16,6 +16,7 @@ public class DataUtils {
                 .avatarAddress(user.getAvatarAddress())
                 .id(user.getId())
                 .tagName(user.getTagName())
+                .username(user.getUserName())
                 .build();
     }
 }
