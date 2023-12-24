0c62c3568fd8754345f4d8172041ac28d9407d6c
DucAnhWorkPlace <leducanh1107.work@gmail.com>
2023-12-24 18:10:30 +0700

    23/12

--- HealthCareApplication/src/main/java/com/example/healthcareapplication/model/dto/CommentDTO.java
+++ HealthCareApplication/src/main/java/com/example/healthcareapplication/model/dto/CommentDTO.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class CommentDTO {
     private Long id;
-    private Long commentId;
     private Long postId;
     private Long userId;
     private Long parentId;
