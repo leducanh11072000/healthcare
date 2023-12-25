1e94fa72e8719897eb82d29f213b1f39c598f3e6
DucAnhWorkPlace <leducanh1107.work@gmail.com>
2023-12-25 23:42:16 +0700

    23/12

--- HealthCareApplication/src/main/java/com/example/healthcareapplication/service/impl/HealthInfoServiceImpl.java
+++ HealthCareApplication/src/main/java/com/example/healthcareapplication/service/impl/HealthInfoServiceImpl.java
@@ -43,7 +43,7 @@ public class HealthInfoServiceImpl implements HealthInfoService {
                 .createTime(now)
                 .status(Common.ACTIVE_STATUS)
                 .build();
-        Double bmiValue = ((double)(healthInfoDTO.getHeight() / (healthInfoDTO.getWeight() * healthInfoDTO.getWeight())) * 10000.0) ;
+        Double bmiValue = (((healthInfoDTO.getHeight()* 10000.0) / (healthInfoDTO.getWeight() * healthInfoDTO.getWeight())) ) ;
         HealthInfo bmi =  HealthInfo.builder()
                 .value(bmiValue)
                 .type(Common.HEALTH_INFO.BMI)
